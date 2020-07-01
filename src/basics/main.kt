2df7566ed5f386391ad0bdcacaf9598c6253bbb9
NihaalWagadia <nwagadia@gmail.com>
2020-06-30 20:12:06 -0400

    Beginner

---
+++ src/basics/main.kt
@@ -0,0 +1,45 @@
+package basics
+import kotlin.random.Random
+
+val PI = "HOLOLO"
+
+fun main(){
+    val stringInput = readLine()!!
+    if(stringInput==null||stringInput.isEmpty()){
+        println("Enter a name")
+    }
+    else{
+        println("$stringInput Buenos Dias")
+    }
+
+   // message+name
+
+    val name = readLine()
+    val noname = if(name==null || name.isEmpty()){
+        PI
+    }
+    else{
+        name
+    }
+
+    val message = if(noname==PI){
+        "NO NAME NO SHAME"
+    }
+    else{
+        "HERE YOU ARE $noname"
+    }
+print(message)
+
+    //create a list of integer from 1..100 and print the value until the value is smaller than 10
+    val random : MutableList<Int> = mutableListOf()
+    for(i in 1..100)
+    {
+        random.add(Random.nextInt(100)+1)
+    }
+    var i=0
+    while(i<random.size && random[i]>10){
+        println("$random[i]")
+        i++
+    }
+}
+
